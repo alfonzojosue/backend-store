7704fff31faa1ea2aa5a15ca125e45e456541018
alfonzojosue <16-00436@usb.ve>
2023-04-06 10:56:49 -0400

    User modification

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,4 +1,40 @@
 import { Sequelize, DataTypes, Model } from 'sequelize';
-
+import { UserAttributes } from './interface/users.interface';
 
 const sequelize = new Sequelize('sqlite::memory:');
+
+class User extends Model<UserAttributes>{}
+
+User.init(
+    {
+    id:{
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull:false,
+    },
+    isAdmin:{
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    banned:{
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    img: {
+        type:DataTypes.STRING,
+        defaultValue: ""
+
+    },
+    deleteAt:{
+        type: DataTypes.STRING,
+    }
+    },
+    {sequelize}
+)
